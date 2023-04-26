1376ee1780cfc950499d47aa117f721cc998f1ab
ADJARO OGAGA <45229900+ogagaCodes@users.noreply.github.com>
2023-04-27 00:44:07 +0100

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
 sudo PM2_HOME=/home/ubuntu/services/.pm2 pm2 list
-cd /home/ubuntu/services/User
+cd /home/ubuntu/services/Users
 # pm2 delete 
 # pm2 --name 
-pm2 restart UserService
+pm2 restart USERS-SERVICE
